4cd4aab4089036b8619e641a13a79c6d8bec05ea
Estudiantes <Estudiantes@Labing70208>
2018-02-14 17:00:01 -0500

    Avance2

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Calculadora {
+
+   
+    public static void main(String[] args) {
+        
+                Principal miCalculadora;
+        miCalculadora = new Principal();
+
+        Scanner miScanner;
+        miScanner = new Scanner(System.in);
+
+        int entrada;
+        System.out.println("Ingresar el valor de X:");
+        entrada = miScanner.nextInt();
+        miCalculadora.setX(entrada);
+        
+        System.out.println("Ingresar el valor de Y:");
+        entrada = miScanner.nextInt();
+        miCalculadora.setY(entrada);
+        
+        miCalculadora.sumar();
+        System.out.println("El resultado de la suma es:");
+        System.out.println(miCalculadora.getZ());
+        
+        miCalculadora.restar();
+        System.out.println("El resultado de la resta es:");
+        System.out.println(miCalculadora.getZ());
+        
+
+    }
+
+}
+
