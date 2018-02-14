97cba3f54f5d181633d8acabe9d04e18def0018d
Estudiantes <Estudiantes@Labing70208>
2018-02-14 17:50:35 -0500

    MiCalculadora Trigonometrica

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,46 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package calculadora;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Estudiantes
- */
+
 public class Calculadora {
 
-   
     public static void main(String[] args) {
-        
-                Principal miCalculadora;
+
+        Principal miCalculadora;
         miCalculadora = new Principal();
 
         Scanner miScanner;
         miScanner = new Scanner(System.in);
 
+          Trigonometrica miTrigonometrica;
+        miTrigonometrica=new Trigonometrica();
+        
         int entrada;
         System.out.println("Ingresar el valor de X:");
         entrada = miScanner.nextInt();
         miCalculadora.setX(entrada);
-        
+
         System.out.println("Ingresar el valor de Y:");
         entrada = miScanner.nextInt();
         miCalculadora.setY(entrada);
-        
+
         miCalculadora.sumar();
         System.out.println("El resultado de la suma es:");
         System.out.println(miCalculadora.getZ());
-        
+
         miCalculadora.restar();
         System.out.println("El resultado de la resta es:");
         System.out.println(miCalculadora.getZ());
         
-
+        miTrigonometrica.coseno();
+        System.out.println("El coseno de z es:");
+        System.out.println(miTrigonometrica.getZ());
+        
+        
     }
 
 }
-
